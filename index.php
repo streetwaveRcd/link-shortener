6febb368cd1c55fa0a0e3c3184245f4446a11cd4
sailesh dabydin <saileshdabydin@gmail.com>
2018-03-26 04:14:27 -0700

    first commit

---
+++ index.php
@@ -0,0 +1,29 @@
+<!-- 
+URL SHORTNER Designed and Developed by HRWNdR
+Developer: Harwinder Singh
+Version: v1.0
+Facebook/iamhrwndr
+Github/hrwndr
+-->
+<?php include 'main.php'; ?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title><?php echo $sitename; ?></title>
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+        <meta name="theme-color" content="#333333">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+        <link href="https://cdn.jsdelivr.net/gh/hrwndr/animate@v1.1/animate.min.css" rel="stylesheet" />
+        <link rel="stylesheet" href="style.css" />
+        <link href="https://fonts.googleapis.com/css?family=Seaweed+Script" rel="stylesheet" />
+        <link rel="icon" href="https://i.imgur.com/MZ8k2Dp.png" />
+    </head>
+    <body class="fade-in-ii">
+        <form class="form-wrapper cf" method="post" action="" >
+            <font size="8" style="font-family: 'Seaweed Script', cursive;" alt="URL SHORTNER by MISTER FREEDOM"><?php echo $sitename; ?></font><p />
+            <input type="text" class="textarea" name="textarea" autocomplete="off" placeholder="Enter Your Link Here..." required>
+            <button type="submit" class="sbtn" name="shrt">SHORT IT!</button>
+        </form>
+        <div class="byline"><p>URL Shortner Designed and Developed by <a href="http://www.facebook.com/misterfreedom.sailesh">MISTER FREEDOM</a></p></div>
+    </body>
+</html>
---
+++ setup.php
@@ -0,0 +1,8 @@
+<?php
+$host = 'localhost'; // Your Host Name
+$dbuser = 'dbuser'; // Database Username
+$dbpass = '7429071s'; // Database password
+$dbname = 'dbname'; // Database Name
+$siteurl = 'http://www.shrten.ml'; // Enter your website url without any slash(/) in the end
+$sitename = 'URL SHORTNER NAME'; // Your Website Name in 8 or less words
+?>
